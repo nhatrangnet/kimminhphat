221c5149c2c9dd83f1dc0652e7fdc51b939cd2d5
LyNguyen <cnttnt@gmail.com>
2024-12-28 19:34:50 +0700

    Revert fix livewire 404

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
-import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
-Livewire.start()
+// import { Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm';
+// Livewire.start()
 
 // Import all of Bootstrap's JS
 import * as Popper from '@popperjs/core';
