d5c8dd82eb2167d5396025419a1328b1ee500e38
LyNguyen <cnttnt@gmail.com>
2025-02-11 17:13:53 +0700

    Add adv

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,16 +1,16 @@
 <h3 class="text-center mb-4">{{ __('Thiết bị vệ sinh', ) }}</h3>
 <div class="block text-center">
 	<a href="http://esslinger.com.vn/" target="blank">
-		<img class="img-thumbnail" src="{{ \Storage::url('adv/esslinger.png') }}" />
+		<img class="img-thumbnail mb-3" src="{{ \Storage::url('adv/esslinger.png') }}" />
 	</a>
 	<a href="https://kaniss.vn/" target="blank">
-		<img class="img-thumbnail" src="{{ \Storage::url('adv/kaniss.png') }}" />
+		<img class="img-thumbnail mb-3" src="{{ \Storage::url('adv/kaniss.png') }}" />
 	</a>
 	<a href="https://vn.toto.com/" target="blank">
-		<img class="img-thumbnail" src="{{ \Storage::url('adv/toto.png') }}" />
+		<img class="img-thumbnail mb-3" src="{{ \Storage::url('adv/toto.png') }}" />
 	</a>
 	<a href="https://www.inax.com.vn/vi/" target="blank" class="m-2">
-		<img class="img-thumbnail" src="{{ \Storage::url('adv/inax.png') }}" />
+		<img class="img-thumbnail mb-3" src="{{ \Storage::url('adv/inax.png') }}" />
 	</a>
 	
 </div>
